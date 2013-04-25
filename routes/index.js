57d989bd7da827807f85ec64f4dafc4d8593b68c
Kyle Stetz <kylestetz@gmail.com>
2013-04-25 08:49:07 -0400

    added comments

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,16 @@
  * GET home page.
  */
 
+// pull in the adjectives and nouns stored in their JS files
 var adjectives = require('./adjectives.js').adjectives;
 var nouns = require('./nouns.js').nouns;
 
+//    index route
 exports.index = function(req, res){
   res.render('index', { sentences: generate(6) });
 };
 
+//    /sentence/:number
 exports.generateSentences = function(req, res){
   var numberOfSentences = 4;
   if(req.params.number) numberOfSentences = req.params.number;
@@ -18,10 +21,13 @@ exports.generateSentences = function(req, res){
   res.send(sentences);
 };
 
+// does the sentence generating
 function generate(numberOfSentences) {
   var sentences = capitalizeFirstLetter(makeSentenceFromTemplate());
   sentences += ". ";
 
+  // if we get a phrase back from randomStartingPhrase()
+  // we do not have to capitalize the first letter of the sentence.
   var cap = true;
   for(var i = 0; i < numberOfSentences - 1; i++) {
     var phrase = randomStartingPhrase();
@@ -34,17 +40,21 @@ function generate(numberOfSentences) {
     sentences += s;
     sentences += ". ";
   }
+
   return sentences;
 }
 
+// get a noun
 function noun() {
   return randomSelection(nouns);
 }
 
+// get an adjective
 function adjective() {
   return randomSelection(adjectives);
 }
 
+// get a noun with either 'a' or 'an' in front of it
 function nounWithAOrAn() {
   var w = noun();
   if(vowelTest(w[0])) {
@@ -54,6 +64,7 @@ function nounWithAOrAn() {
   }
 }
 
+// get an adjective with either 'a' or 'an' in front of it
 function adjectiveWithAOrAn() {
   var w = adjective();
   if(vowelTest(w[0])) {
@@ -63,6 +74,7 @@ function adjectiveWithAOrAn() {
   }
 }
 
+// is it a vowel?
 function vowelTest(s) {
   return s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u';
 }
@@ -72,6 +84,7 @@ function capitalizeFirstLetter(string)
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// returns a starting phrase about half the time, otherwise it's empty
 function randomStartingPhrase() {
   if(Math.random() < 0.5) {
     return phrases[ Math.ceil(Math.random() * phrases.length) ];
@@ -79,10 +92,59 @@ function randomStartingPhrase() {
   return "";
 }
 
+// take an array and return a random selection from it
 function randomSelection(l) {
   return l[Math.ceil(Math.random() * l.length - 1)];
 }
 
+// ======================= TEMPLATES ==========================
+// %a%        : adjective
+// %n%        : noun
+// (a/an) %a% : adjective with a or an in front
+// (a/an) %n% : noun with a or an in front
+
+function makeSentenceFromTemplate() {
+  // pick a template
+  var t = randomSelection(sentenceTemplates);
+
+  t = replaceAllOccurrences(t, "\\(a/an\\) %n%", nounWithAOrAn);
+  t = replaceAllOccurrences(t, "\\(a/an\\) %a%", adjectiveWithAOrAn);
+  t = replaceAllOccurrences(t, "%n%", noun);
+  t = replaceAllOccurrences(t, "%a%", adjective);
+  return t;
+}
+
+// here's where the magic happens. We're using regular expressions so that
+// in the future we can get pretty complicated with our templates.
+function replaceAllOccurrences(template, regexString, generator) {
+  // count the instances of the expression in the template
+  var count = template.match(new RegExp(regexString, 'g'));
+  var modifiedTemplate = template;
+  // for each instance of the expression, replace it with whatever generator was passed in
+  if(count && count.length) {
+    for(var i = 0; i < count.length; i++){
+      modifiedTemplate = modifiedTemplate.replace(new RegExp(regexString), generator());
+    }
+  }
+  return modifiedTemplate;
+}
+
+// style guide: no periods, no first capital letters.
+var sentenceTemplates = [
+  "the %n% is (a/an) %n%",
+  "(a/an) %n% is (a/an) %a% %n%",
+  "the first %a% %n% is, in its own way, (a/an) %n%",
+  "their %n% was, in this moment, (a/an) %a% %n%",
+  "(a/an) %n% is (a/an) %n% from the right perspective",
+  "the literature would have us believe that (a/an) %a% %n% is not but (a/an) %n%",
+  "(a/an) %a% %n% is (a/an) %n% of the mind",
+  "the %a% %n% reveals itself as (a/an) %a% %n% to those who look",
+  "authors often misinterpret the %n% as (a/an) %a% %n%, when in actuality it feels more like (a/an) %a% %n%",
+  "we can assume that any instance of (a/an) %n% can be construed as (a/an) %a% %n%",
+  "they were lost without the %a% %n% that composed their %n%"
+];
+
+// partial phrases to start with. Capitalized.
 var phrases = [
   "To be more specific, ",
   "In recent years, ",
@@ -101,53 +163,4 @@ var phrases = [
   "The pedantic academic would easily confuse the idea that ",
   "The zeitgeist contends that ",
   "Though we assume the latter, "
-];
-
-// TEMPLATES
-// A noun is an adjective noun.
-//   (a/an) %n is (a/an) %a %n.
-// The most %a %n is, in its own way, (a/an) %n.
-// Their %n was, in this moment, (a/an) %a %n.
-
-function makeSentenceFromTemplate() {
-  // pick a template
-  var t = randomSelection(sentenceTemplates);
-  // take it apart
-  // console.log(replaceAllOccurrences("(a/an) %n% is (a/an) %n% from the right perspective.", "\\(a/an\\) %n%", nounWithAOrAn));
-  t = replaceAllOccurrences(t, "\\(a/an\\) %n%", nounWithAOrAn);
-  t = replaceAllOccurrences(t, "\\(a/an\\) %a%", adjectiveWithAOrAn);
-  // t = t.replace(/\(a\/an\) %a%/, adjectiveWithAOrAn());
-  t = replaceAllOccurrences(t, "%n%", noun);
-  t = replaceAllOccurrences(t, "%a%", adjective);
-  // t = t.replace(/%n%/, noun());
-  // t = t.replace(/%a%/, adjective());
-  return t;
-}
-
-function replaceAllOccurrences(template, regexString, generator) {
-  var count = template.match(new RegExp(regexString, 'g'));
-  var modifiedTemplate = template;
-  if(count && count.length) {
-    for(var i = 0; i < count.length; i++){
-      modifiedTemplate = modifiedTemplate.replace(new RegExp(regexString), generator());
-    }
-  }
-  return modifiedTemplate;
-}
-
-// style guide: no periods, no first capital letters. we can add these things dynamically
-// when we introduce partial phrases.
-
-var sentenceTemplates = [
-  "the %n% is (a/an) %n%",
-  "(a/an) %n% is (a/an) %a% %n%",
-  "the first %a% %n% is, in its own way, (a/an) %n%",
-  "their %n% was, in this moment, (a/an) %a% %n%",
-  "(a/an) %n% is (a/an) %n% from the right perspective",
-  "the literature would have us believe that (a/an) %a% %n% is not but (a/an) %n%",
-  "(a/an) %a% %n% is (a/an) %n% of the mind",
-  "the %a% %n% reveals itself as (a/an) %a% %n% to those who look",
-  "authors often misinterpret the %n% as (a/an) %a% %n%, when in actuality it feels more like (a/an) %a% %n%",
-  "we can assume that any instance of (a/an) %n% can be construed as (a/an) %a% %n%",
-  "they were lost without the %a% %n% that composed their %n%"
 ];
