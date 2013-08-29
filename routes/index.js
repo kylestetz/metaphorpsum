a23e6c2fbc2e8d751eea3eba6fe2a51361e2ff0e
Kyle Stetz <kylestetz@gmail.com>
2013-08-29 17:21:23 -0400

    refactor! added Sentencer, a new sentence templating setup. Also added plurals!

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
-// pull in the adjectives and nouns stored in their JS files
-var adjectives = require('./adjectives.js').adjectives;
+var sentencer = require('./sentencer.js');
+
 var nouns = require('./nouns.js').nouns;
+var adjectives = require('./adjectives.js').adjectives;
+
+sentencer.setNouns(nouns);
+sentencer.setAdjectives(adjectives);
 
 function constrain(input, max) {
   if(input > max) {
@@ -59,7 +63,7 @@ exports.generateParagraphs = function(req, res) {
   res.send(paragraphString);
 }
 
-//    /sentence/:number
+//    /sentences/:number
 exports.generateSentences = function(req, res){
   var numberOfSentences = req.params.number || 4;
   var sentences = generate(numberOfSentences);
@@ -99,49 +103,17 @@ function generate(numberOfSentences) {
   return sentences;
 }
 
-// get a noun
-function noun() {
-  return randomSelection(nouns);
+function makeSentenceFromTemplate() {
+  return  sentencer.make( randomSelection(sentenceTemplates) );
 }
 
-// get an adjective
-function adjective() {
-  return randomSelection(adjectives);
-}
-
-// get a noun with either 'a' or 'an' in front of it
-function nounWithAOrAn() {
-  var w = noun();
-  if(vowelTest(w[0])) {
-    return "an " + w;
-  } else {
-    return "a " + w;
-  }
-}
-
-// get an adjective with either 'a' or 'an' in front of it
-function adjectiveWithAOrAn() {
-  var w = adjective();
-  if(vowelTest(w[0])) {
-    return "an " + w;
-  } else {
-    return "a " + w;
-  }
-}
-
-// is it a vowel?
-function vowelTest(s) {
-  return s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u';
-}
-
-function capitalizeFirstLetter(string)
-{
+function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // returns a starting phrase about half the time, otherwise it's empty
 function randomStartingPhrase() {
-  if(Math.random() < 0.45) {
+  if(Math.random() < 0.4) {
     return phrases[ Math.ceil(Math.random() * phrases.length) ];
   }
   return "";
@@ -152,63 +124,33 @@ function randomSelection(l) {
   return l[Math.ceil(Math.random() * l.length - 1)];
 }
 
-// ======================= TEMPLATES ==========================
-// %a%        : adjective
-// %n%        : noun
-// (a/an) %a% : adjective with a or an in front
-// (a/an) %n% : noun with a or an in front
-
-function makeSentenceFromTemplate() {
-  // pick a template
-  var t = randomSelection(sentenceTemplates);
-
-  // it's important that we check for "(a/an) %n%" before we check for %n% or we might have some false matches
-  t = replaceAllOccurrences(t, "\\(a/an\\) %n%", nounWithAOrAn);
-  t = replaceAllOccurrences(t, "\\(a/an\\) %a%", adjectiveWithAOrAn);
-  // now we can check for the solo guys
-  t = replaceAllOccurrences(t, "%n%", noun);
-  t = replaceAllOccurrences(t, "%a%", adjective);
-  return t;
-}
-
-// here's where the magic happens. We're using regular expressions so that
-// in the future we can get pretty complicated with our templates.
-function replaceAllOccurrences(template, regexString, generator) {
-  // count the instances of the expression in the template
-  var count = template.match(new RegExp(regexString, 'g'));
-  var modifiedTemplate = template;
-  // for each instance of the expression, replace it with whatever generator was passed in
-  if(count && count.length) {
-    for(var i = 0; i < count.length; i++){
-      modifiedTemplate = modifiedTemplate.replace(new RegExp(regexString), generator());
-    }
-  }
-  return modifiedTemplate;
-}
-
 // style guide: no periods, no first capital letters.
 var sentenceTemplates = [
-  "the %n% is (a/an) %n%",
-  "(a/an) %n% is (a/an) %a% %n%",
-  "the first %a% %n% is, in its own way, (a/an) %n%",
-  "their %n% was, in this moment, (a/an) %a% %n%",
-  "(a/an) %n% is (a/an) %n% from the right perspective",
-  "the literature would have us believe that (a/an) %a% %n% is not but (a/an) %n%",
-  "(a/an) %a% %n% is (a/an) %n% of the mind",
-  "the %a% %n% reveals itself as (a/an) %a% %n% to those who look",
-  "authors often misinterpret the %n% as (a/an) %a% %n%, when in actuality it feels more like (a/an) %a% %n%",
-  "we can assume that any instance of (a/an) %n% can be construed as (a/an) %a% %n%",
-  "they were lost without the %a% %n% that composed their %n%",
-  "the %a% %n% comes from (a/an) %a% %n%",
-  "(a/an) %n% can hardly be considered (a/an) %a% %n% without also being (a/an) %n%",
-  "few can name (a/an) %a% %n% that isn't (a/an) %a% %n%",
-  "some posit the %a% %n% to be less than %a%",
-  "(a/an) %n% of the %n% is assumed to be (a/an) %a% %n%",
-  "(a/an) %n% sees (a/an) %n% as (a/an) %a% %n%",
-  "the %n% of (a/an) %n% becomes (a/an) %a% %n%",
-  "(a/an) %n% is (a/an) %n%'s %n%",
-  "(a/an) %n% is the %n% of (a/an) %n%",
-  "(a/an) %a% %n%'s %n% comes with it the thought that the %a% %n% is (a/an) %n%"
+  "the {{ noun }} is {{ a_noun }}",
+  "{{ a_noun }} is {{ an_adjective }} {{ noun }}",
+  "the first {{ adjective }} {{ noun }} is, in its own way, {{ a_noun }}",
+  "their {{ noun }} was, in this moment, {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} is {{ a_noun }} from the right perspective",
+  "the literature would have us believe that {{ an_adjective }} {{ noun }} is not but {{ a_noun }}",
+  "{{ an_adjective }} {{ noun }} is {{ a_noun }} of the mind",
+  "the {{ adjective }} {{ noun }} reveals itself as {{ an_adjective }} {{ noun }} to those who look",
+  "authors often misinterpret the {{ noun }} as {{ an_adjective }} {{ noun }}, when in actuality it feels more like {{ an_adjective}} {{ noun }}",
+  "we can assume that any instance of {{ a_noun }} can be construed as {{ an_adjective }} {{ noun }}",
+  "they were lost without the {{ adjective }} {{ noun }} that composed their {{ noun }}",
+  "the {{ adjective }} {{ noun }} comes from {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} can hardly be considered {{ an_adjective }} {{ noun }} without also being {{ a_noun }}",
+  "few can name {{ an_adjective }} {{ noun }} that isn't {{ an_adjective }} {{ noun }}",
+  "some posit the {{ adjective }} {{ noun }} to be less than {{ adjective }}",
+  "{{ a_noun }} of the {{ noun }} is assumed to be {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} sees {{ a_noun }} as {{ an_adjective }} {{ noun }}",
+  "the {{ noun }} of {{ a_noun }} becomes {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} is {{ a_noun }}'s {{ noun }}",
+  "{{ a_noun }} is the {{ noun }} of {{ a_noun }}",
+  "{{ an_adjective }} {{ noun }}'s {{ noun }} comes with it the thought that the {{ adjective }} {{ noun }} is {{ a_noun }}",
+  "{{ nouns }} are {{ adjective }} {{ nouns }}",
+  "{{ adjective }} {{ nouns }} show us how {{ nouns }} can be {{ nouns }}",
+  "before {{ nouns }}, {{ nouns }} were only {{ nouns }}",
+  "those {{ nouns }} are nothing more than {{ nouns }}"
 ];
 
 // partial phrases to start with. Capitalized.
